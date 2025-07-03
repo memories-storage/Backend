faac2d424821f39948cff9e5aa3f6e54891d3b62
Ved Prakash <vedprakash182001@gmail.com>
2025-07-04 00:56:51 +0530

    url updated

--- internal/routes/image.go
+++ internal/routes/image.go
@@ -8,7 +8,7 @@ import (
 
 func RegisterImageRoutes(r chi.Router) {
 	// Public route for adding an image
-	r.Post("/addImages/{userId}", handlers.AddImageHandler)
+	r.Post("/upload/files", handlers.AddImageHandler)
 	r.Delete("/deleteImages/{id}", handlers.DeleteImageHandler)
 
 	r.Group(func(protected chi.Router) {
