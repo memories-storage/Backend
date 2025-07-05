9fc530792a2ff180c8f567dcfcc4c4d8b413b775
rider6253 <patelsumit.1652002@gmail.com>
2025-07-06 01:53:24 +0530

    changed qr api

--- internal/utils/QRCode.go
+++ internal/utils/QRCode.go
@@ -9,7 +9,7 @@ import (
 )
 
 func GenerateQRCode(id string) (string, error) {
-	content := fmt.Sprintf("http://localhost:3000/upload/id=%s",id)
+	content := fmt.Sprintf("https://weddingnew.netlify.app/upload/id=%s",id)
 	savePath := fmt.Sprintf("%s.png", id)
 
 	// Ensure the directory exists
